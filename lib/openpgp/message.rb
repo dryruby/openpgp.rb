f223572cc9cc35ee9778abe1a4b2754230da6b3f
Arto Bendiken <arto@bendiken.net>
2009-04-16 12:35:18 +0200

    Fleshed out Message and Packet classes.

--- lib/openpgp/message.rb
+++ lib/openpgp/message.rb
@@ -12,7 +12,29 @@ module OpenPGP
     # @see http://tools.ietf.org/html/rfc4880#section-4.1
     # @see http://tools.ietf.org/html/rfc4880#section-4.2
     def self.parse(data)
+      packet = self.new
       # TODO
+      packet
+    end
+
+    def initialize(packets = [])
+      @packets = packets
+    end
+
+    def each(&block)
+      packets.each(&block)
+    end
+
+    def <<(packet)
+      packets << packet
+    end
+
+    def empty?
+      packets.empty?
+    end
+
+    def size
+      inject(0) { |sum, packet| sum + packet.size }
     end
   end
 end
--- lib/openpgp/packet.rb
+++ lib/openpgp/packet.rb
@@ -5,6 +5,9 @@ module OpenPGP
   # @see http://tools.ietf.org/html/rfc4880#section-4.1
   # @see http://tools.ietf.org/html/rfc4880#section-4.3
   class Packet
+    attr_accessor :tag
+    attr_accessor :size
+
     ##
     # @see http://tools.ietf.org/html/rfc4880#section-4.2
     def self.parse(data)
@@ -21,6 +24,10 @@ module OpenPGP
     # @see http://tools.ietf.org/html/rfc4880#section-4.2.1
     def self.parse_old_format(data)
       # TODO
+    end
+
+    def initialize(tag = nil, size = 0)
+      @tag, @size = tag, size
     end
   end
 end
