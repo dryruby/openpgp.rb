ea7c3e6980201674f2f232e371559d4b1653b5a1
Arto Bendiken <arto@bendiken.net>
2009-04-16 08:59:48 +0200

    Improved parameter name consistency.

--- lib/openpgp/armor.rb
+++ lib/openpgp/armor.rb
@@ -3,14 +3,14 @@ module OpenPGP
   # @see http://tools.ietf.org/html/rfc4880#section-6
   # @see http://tools.ietf.org/html/rfc4880#section-6.2
   # @see http://tools.ietf.org/html/rfc2045
-  def self.enarmor(data, header = nil, headers = [])
+  def self.enarmor(data, marker = nil, headers = [])
     # TODO
   end
 
   ##
   # @see http://tools.ietf.org/html/rfc4880#section-6
   # @see http://tools.ietf.org/html/rfc2045
-  def self.dearmor(text, header = nil)
+  def self.dearmor(text, marker = nil)
     # TODO
   end
 
