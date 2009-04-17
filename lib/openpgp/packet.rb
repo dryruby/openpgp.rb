6abfee738079e67d558c5767ce8a7a6a7a2d3178
Arto Bendiken <arto@bendiken.net>
2009-04-17 11:15:31 +0200

    Implemented packet class stubs.

--- lib/openpgp/packet.rb
+++ lib/openpgp/packet.rb
@@ -79,6 +79,122 @@ module OpenPGP
     end
 
     ##
+    # OpenPGP Public-Key Encrypted Session Key packet (tag 1).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.1
+    class AsymmetricSessionKey < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP Signature packet (tag 2).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.2
+    class Signature < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP Symmetric-Key Encrypted Session Key packet (tag 3).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.3
+    class SymmetricSessionKey < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP One-Pass Signature packet (tag 4).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.4
+    class OnePassSignature < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP Public-Key packet (tag 6).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.5.1.1
+    # @see http://tools.ietf.org/html/rfc4880#section-5.5.2
+    # @see http://tools.ietf.org/html/rfc4880#section-11.1
+    # @see http://tools.ietf.org/html/rfc4880#section-12
+    class PublicKey < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP Public-Subkey packet (tag 14).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.5.1.2
+    # @see http://tools.ietf.org/html/rfc4880#section-5.5.2
+    # @see http://tools.ietf.org/html/rfc4880#section-11.1
+    # @see http://tools.ietf.org/html/rfc4880#section-12
+    class PublicSubkey < PublicKey
+      # TODO
+    end
+
+    ##
+    # OpenPGP Secret-Key packet (tag 5).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.5.1.3
+    # @see http://tools.ietf.org/html/rfc4880#section-5.5.3
+    # @see http://tools.ietf.org/html/rfc4880#section-11.2
+    # @see http://tools.ietf.org/html/rfc4880#section-12
+    class SecretKey < PublicKey
+      # TODO
+    end
+
+    ##
+    # OpenPGP Secret-Subkey packet (tag 7).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.5.1.4
+    # @see http://tools.ietf.org/html/rfc4880#section-5.5.3
+    # @see http://tools.ietf.org/html/rfc4880#section-11.2
+    # @see http://tools.ietf.org/html/rfc4880#section-12
+    class SecretSubkey < SecretKey
+      # TODO
+    end
+
+    ##
+    # OpenPGP Compressed Data packet (tag 8).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.6
+    class CompressedData < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP Symmetrically Encrypted Data packet (tag 9).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.7
+    class EncryptedData < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP Marker packet (tag 10).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.8
+    class Marker < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP Literal Data packet (tag 11).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.9
+    class LiteralData < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP Trust packet (tag 12).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.10
+    class Trust < Packet
+      # TODO
+    end
+
+    ##
     # OpenPGP User ID packet (tag 13).
     #
     # @see http://tools.ietf.org/html/rfc4880#section-5.11
@@ -115,11 +231,64 @@ module OpenPGP
       end
     end
 
+    ##
+    # OpenPGP User Attribute packet (tag 17).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.12
+    # @see http://tools.ietf.org/html/rfc4880#section-11.1
+    class UserAttribute < Packet
+      attr_accessor :packets
+
+      # TODO
+    end
+
+    ##
+    # OpenPGP Sym. Encrypted Integrity Protected Data packet (tag 18).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.13
+    class IntegrityProtectedData < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP Modification Detection Code packet (tag 19).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-5.14
+    class ModificationDetectionCode < Packet
+      # TODO
+    end
+
+    ##
+    # OpenPGP Private or Experimental packet (tags 60..63).
+    #
+    # @see http://tools.ietf.org/html/rfc4880#section-4.3
+    class Experimental < Packet; end
+
     protected
       ##
       # @see http://tools.ietf.org/html/rfc4880#section-4.3
       @@tags = {
-        13 => UserID,
+         1 => AsymmetricSessionKey,      # Public-Key Encrypted Session Key
+         2 => Signature,                 # Signature Packet
+         3 => SymmetricSessionKey,       # Symmetric-Key Encrypted Session Key Packet
+         4 => OnePassSignature,          # One-Pass Signature Packet
+         5 => SecretKey,                 # Secret-Key Packet
+         6 => PublicKey,                 # Public-Key Packet
+         7 => SecretSubkey,              # Secret-Subkey Packet
+         8 => CompressedData,            # Compressed Data Packet
+         9 => EncryptedData,             # Symmetrically Encrypted Data Packet
+        10 => Marker,                    # Marker Packet
+        11 => LiteralData,               # Literal Data Packet
+        12 => Trust,                     # Trust Packet
+        13 => UserID,                    # User ID Packet
+        14 => PublicSubkey,              # Public-Subkey Packet
+        17 => UserAttribute,             # User Attribute Packet
+        18 => IntegrityProtectedData,    # Sym. Encrypted and Integrity Protected Data Packet
+        19 => ModificationDetectionCode, # Modification Detection Code Packet
+        60 => Experimental,              # Private or Experimental Values
+        61 => Experimental,              # Private or Experimental Values
+        62 => Experimental,              # Private or Experimental Values
+        63 => Experimental,              # Private or Experimental Values
       }
   end
 end
