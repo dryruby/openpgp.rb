69251c5d263f8fdbdf29bb80c3573e137843da56
Arto Bendiken <arto@bendiken.net>
2009-04-30 06:44:30 +0200

    Added sanity check for OpenPGP.bitlength().

--- lib/openpgp/util.rb
+++ lib/openpgp/util.rb
@@ -39,6 +39,6 @@ module OpenPGP
   #
   # @see http://tools.ietf.org/html/rfc4880#section-3.2
   def self.bitlength(data)
-    (data.size - 1) * 8 + (Math.log(data[0]) / Math.log(2)).floor + 1
+    data.empty? ? 0 : (data.size - 1) * 8 + (Math.log(data[0]) / Math.log(2)).floor + 1
   end
 end
