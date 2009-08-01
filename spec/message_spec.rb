16f7cdd0a22ebac70254ed922cbbc3df8e6bf550
Arto Bendiken <arto@bendiken.net>
2009-08-01 17:33:29 +0200

    Implemented more specs for OpenPGP message parsing.

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -10,10 +10,12 @@ describe OpenPGP::Message, " at <http://ar.to/pgp.txt>" do
       lambda { @binary = OpenPGP.dearmor(@ascii) }.should_not raise_error
       @binary.should_not be_empty
       @binary.should be_a_kind_of(String)
-      @binary.should have(1939).characters
+      @binary.should have(1_939).characters
     end
 
-    it "should match the CRC24 checksum of ..." # TODO
+    it "should have the CRC24 checksum of 0x3B1080" do
+      lambda { @binary = OpenPGP.dearmor(@ascii, nil, :crc => true) }.should_not raise_error
+    end
   end
 
   context "when parsed" do
@@ -23,8 +25,20 @@ describe OpenPGP::Message, " at <http://ar.to/pgp.txt>" do
       @message.packets.should have(9).items
     end
 
-    it "should contain a public key packet"     # TODO
-    it "should contain a public subkey packet"  # TODO
-    it "should contain three user ID packets"   # TODO
+    it "should contain a public key packet" do
+      @message = OpenPGP::Message.parse(OpenPGP.dearmor(@ascii))
+      @message.map(&:class).should include(OpenPGP::Packet::PublicKey)
+    end
+
+    it "should contain a public subkey packet" do
+      @message = OpenPGP::Message.parse(OpenPGP.dearmor(@ascii))
+      @message.map(&:class).should include(OpenPGP::Packet::PublicSubkey)
+    end
+
+    it "should contain three user ID packets" do
+      @message = OpenPGP::Message.parse(OpenPGP.dearmor(@ascii))
+      @message.map(&:class).should include(OpenPGP::Packet::UserID)
+      @message.find_all { |packet| packet.is_a?(OpenPGP::Packet::UserID) }.should have(3).items
+    end
   end
 end
