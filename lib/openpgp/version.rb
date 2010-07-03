d841fb65d08bb194b0af2e13bc8f29f8fdb9bb28
Arto Bendiken <arto@bendiken.net>
2010-07-03 08:08:03 +0200

    Simplified OpenPGP::VERSION::STRING construction.

--- lib/openpgp/version.rb
+++ lib/openpgp/version.rb
@@ -5,10 +5,18 @@ module OpenPGP
     TINY  = 2
     EXTRA = nil
 
-    STRING = [MAJOR, MINOR, TINY].join('.')
-    STRING << "-#{EXTRA}" if EXTRA
+    STRING = [MAJOR, MINOR, TINY, EXTRA].compact.join('.')
 
-    def self.to_s()   STRING end
+    ##
+    # @return [String]
+    def self.to_s() STRING end
+
+    ##
+    # @return [String]
     def self.to_str() STRING end
+
+    ##
+    # @return [Array(Integer, Integer, Integer)]
+    def self.to_a() [MAJOR, MINOR, TINY] end
   end
 end
